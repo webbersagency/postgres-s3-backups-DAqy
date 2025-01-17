4d388a8700747597faa1bc10e86f48fe79e988a8
Percy <percy@railway.app>
2025-01-17 16:41:11 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM node:20.11.1-alpine AS build
+
+ENV NPM_CONFIG_UPDATE_NOTIFIER=false
+ENV NPM_CONFIG_FUND=false
+
+WORKDIR /app
+
+COPY package*.json tsconfig.json ./
+COPY src ./src
+
+RUN npm ci && \
+    npm run build && \
+    npm prune --production
+
+FROM node:20.11.1-alpine
+
+WORKDIR /app
+
+COPY --from=build /app/node_modules ./node_modules
+COPY --from=build /app/dist ./dist
+COPY --from=build /app/package.json ./
+
+ARG PG_VERSION='16'
+
+RUN apk add --update --no-cache postgresql${PG_VERSION}-client
+
+CMD pg_isready --dbname=$BACKUP_DATABASE_URL && \
+    pg_dump --version && \
+    node dist/index.js
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { CronJob } from "cron";
+import { backup } from "./backup.js";
+import { env } from "./env.js";
+
+console.log("NodeJS Version: " + process.version);
+
+const tryBackup = async () => {
+  try {
+    await backup();
+  } catch (error) {
+    console.error("Error while running backup: ", error);
+    process.exit(1);
+  }
+}
+
+if (env.RUN_ON_STARTUP || env.SINGLE_SHOT_MODE) {
+  console.log("Running on start backup...");
+
+  await tryBackup();
+
+  if (env.SINGLE_SHOT_MODE) {
+    console.log("Database backup complete, exiting...");
+    process.exit(0);
+  }
+}
+
+const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
+  await tryBackup();
+});
+
+job.start();
+
+console.log("Backup cron scheduled...");
